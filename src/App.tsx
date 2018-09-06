6a20d91b9dfb8c3cf71624bb040a17b1907e2f1c
Josh <jdegraw42@gmail.com>
2018-09-06 14:56:08 -0600

    Updated readme and App

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import './App.css';
 
 import { Jumbotron, UncontrolledTooltip } from './reactstrap';
 
-class App extends React.Component {
+class App extends React.Component<{},{switched: boolean;}> {
+  public readonly state ={switched: false}
   private ref1 = React.createRef<HTMLAnchorElement>();
+
+
+  public componentDidMount(){
+    setTimeout(()=>this.setState({switched:true}),5000);
+  }
 
   public render() {
     return (
       <Jumbotron>
           <h1 className="App-title">Ref tester</h1>
           <p>
-            <a href="#" ref={this.ref1}>This uses a ref object</a>          
+            <a href="#" ref={this.state.switched? undefined: this.ref1}>This uses a ref object</a>  
+            <br/>        
+            <a href="#" ref={this.state.switched? this.ref1: undefined}>This will have the tooltip after a few seconds</a>          
             <UncontrolledTooltip target={this.ref1}>Hello</UncontrolledTooltip>
           </p>
       </Jumbotron>
